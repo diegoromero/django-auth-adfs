36b2dfcbbc625601a9c89384b0ae8384573e58ac
diego <diego@keblarsolutions.com>
2022-04-29 15:46:33 +0200

    adfs

--- django_auth_adfs/backend.py
+++ django_auth_adfs/backend.py
@@ -284,8 +284,6 @@ class AdfsAuthCodeBackend(AdfsBaseBackend):
         adfs_response = self.exchange_auth_code(authorization_code, request)
         access_token = adfs_response["access_token"]
         user = self.process_access_token(access_token, adfs_response)
-        logger.debug("AdfsAuthCodeBackend")
-        logger.debug(user)
         return user
 
 
